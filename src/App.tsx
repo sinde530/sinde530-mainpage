00f90cb089e9c516e7f3ab7c00edc6fcf49d9e47
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-10-11 15:12:19 +0900

    Add Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,59 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div({
+  padding: '20px',
+  minHeight: '100vh',
+});
+
+const HeaderContainer = styled.div({
+  backgroundColor: '#333',
+  padding: '10px',
+  color: 'white',
+  textAlign: 'center',
+});
+
+const FlexContainer = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  gap: '10px',
+});
+
+const SidebarContainer = styled.div({
+  backgroundColor: '#ddd',
+  minWidth: '340px',
+  maxWidth: '340px%',
+  padding: '10px',
+  color: '#000',
+});
+
+const ContentContainer = styled.div({
+  width: '100%',
+  padding: '10px',
+  backgroundColor: '#eee',
+  color: '#000',
+});
+
+const FooterContainer = styled.div({
+  backgroundColor: '#555',
+  padding: '10px',
+  color: 'white',
+  textAlign: 'center',
+  marginTop: '20px',
+});
+
 export default function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container>
+      <HeaderContainer>
+        <h1>Vite + React</h1>
+      </HeaderContainer>
+
+      <FlexContainer>
+        <SidebarContainer>Sidebar</SidebarContainer>
+        <ContentContainer>Content</ContentContainer>
+      </FlexContainer>
+
+      <FooterContainer>Footer</FooterContainer>
+    </Container>
   );
 }
