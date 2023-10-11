d4f69964a36473940b67795c954c005854f71aac
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-10-11 15:30:22 +0900

    Add Footer Component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const FooterContainer = styled.div({
+  backgroundColor: '#555',
+  padding: '10px',
+  color: 'white',
+  textAlign: 'center',
+  // marginTop: '20px',
+});
+
+export default function Footer() {
+  return <FooterContainer>Footer</FooterContainer>;
+}
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
+import Footer from 'src/components/Footer';
 import Header from 'src/components/Header';
 
 const Container = styled.div({
@@ -20,24 +21,17 @@ const FlexContainer = styled.div({
 //   color: '#000',
 // });
 
-const FooterContainer = styled.div({
-  backgroundColor: '#555',
-  padding: '10px',
-  color: 'white',
-  textAlign: 'center',
-  // marginTop: '20px',
-});
-
 export default function MainLayout() {
   return (
     <Container>
       <Header />
+
       <FlexContainer>
         {/* <SidebarContainer>Sidebar</SidebarContainer> */}
         <Outlet />
       </FlexContainer>
 
-      <FooterContainer>Footer</FooterContainer>
+      <Footer />
     </Container>
   );
 }
