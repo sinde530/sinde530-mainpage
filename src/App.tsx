bbfeb37589183390e1d7539689715db656f07429
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-10-11 15:48:43 +0900

    Add Routes SignIn, SignUp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 export default function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/account/service/signin" element={<SignIn />} />
+        <Route path="/account/service/create/signup" element={<SignUp />} />
       </Route>
     </Routes>
   );
