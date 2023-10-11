9db88e606b13d348030c2b808ec32e552d9589c0
Sinde530 <70146617+sinde530@users.noreply.github.com>
2023-10-11 15:45:31 +0900

    Add Header Styled

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,49 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 const HeaderContainer = styled.div({
-  backgroundColor: '#333',
-  padding: '10px',
-  color: 'white',
+  backgroundColor: '#ffffff',
   textAlign: 'center',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  borderBottom: '1px solid #DDDDDD',
+});
+
+const Box = styled.div({
+  display: 'flex',
+});
+
+const LogoText = styled.h1({
+  color: '#000',
+  padding: '16px 16px',
+  fontSize: '18px',
+});
+
+const LinkTo = styled(Link)({
+  margin: '16px 16px',
+});
+
+const Text = styled.p({
+  color: '#000',
 });
 
 export default function Header() {
   return (
     <HeaderContainer>
-      <h1>Vite + React</h1>
+      <Box>
+        <Link to="/">
+          <LogoText>Sinde530</LogoText>
+        </Link>
+      </Box>
+      <Box>
+        <LinkTo to="/account/service/signin">
+          <Text>로그인</Text>
+        </LinkTo>
+        <LinkTo to="/account/service/create/signup">
+          <Text>회원가입</Text>
+        </LinkTo>
+      </Box>
     </HeaderContainer>
   );
 }
